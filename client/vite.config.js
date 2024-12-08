f47bacd19e300ae231106e6784438031a7dcbfd7
MSUResult <adarshrajput9040@gmail.com>
2024-12-08 08:28:38 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer'; // Correct import
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 500, // Custom chunk size warning limit
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Split node_modules into separate chunks for each dependency
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString();
-          }
-        },
-      },
-      plugins: [
-        visualizer({ open: true }), // Correct usage of visualizer plugin
-      ],
-    },
-  },
 });
